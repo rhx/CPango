8e7487d93c988f268006910abf4a50ffd6d7213b
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-13 09:36:58 +1000

    Update to Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,3 +1,11 @@
+// swift-tools-version:4.0
+
 import PackageDescription 
 
-let package = Package(name: "CPango")
+let package = Package(name: "CPango",
+	pkgConfig: "pango",
+	providers: [
+		.brew(["pango", "glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libpango1.0-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	]
+)
